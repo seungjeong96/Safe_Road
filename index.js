d9665e26c2b4a474fcaa66ebd3db3a4d8c8471b4
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-11-28 19:54:20 +0900

    feat: implement filesystem

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require("express");
 const http = require("http");
 const cors = require("cors");
 const app = express();
+const fs = require("fs");
+const path = require("path");
+const { SerialPort } = require("serialport");
+const { ReadlineParser } = require("@serialport/parser-readline");
+const portName = "COM7";
+
+const arduinoSerialPort = new SerialPort({
+  path: portName,
+  baudRate: 115200,
+});
+
+const parser = new ReadlineParser();
+arduinoSerialPort.pipe(parser);
 
 // cors 방지를 위한 미들웨어 적용
 app.use(cors());
@@ -36,32 +49,52 @@ const io = new Server(server, {
   },
 });
 
-// 웹소켓 연결 시작
-// 초기 path는 /watch4로 설정함
-const socketWatch4 = io.of("/watch4");
-socketWatch4.on("connection", (socket) => {
-  console.log(`watch4 웹 소켓 연결됨`);
-  socket.on("hrate", (msg) => {
-    console.log(msg);
-    webpage.emit("hrate", msg);
-    // 로직 구현
-  });
-
-  const webpage = io.of("/webpage");
-  webpage.on("connect", (socket) => {
-    console.log("웹페이지 웹소켓 연결됨");
-  });
-
-  /* 가속도
-  socket.on("acc", (msg) => {
-    console.log(msg);
-  });
-  */
-
+io.on("connection", (socket) => {
+  console.log("a user connected");
   socket.on("disconnect", () => {
-    console.log("watch4 웹 소켓 연결 끊김");
+    console.log("user disconnected");
+  });
+
+  socket.emit("result", "${socket.id}로 연결되었습니다.");
+  parser.on("data", (data) => {
+    console.log(data);
+    socket.emit("data", data);
+  });
+
+  socket.on("message", (msg) => {
+    //받고
+    console.log("클라이언트의 요청이 있습니다.");
+    console.log(msg);
+    socket.emit("result", `수신된 메세지는 "${msg}" 입니다.`);
   });
 });
+// 웹소켓 연결 시작
+// 초기 path는 /watch4로 설정함
+
+// const socketWatch4 = io.of("/watch4");
+// socketWatch4.on("connection", (socket) => {
+//   console.log(`watch4 웹 소켓 연결됨`);
+//   socket.on("hrate", (msg) => {
+//     console.log(msg);
+//     webpage.emit("hrate", msg);
+//     // 로직 구현
+//   });
+
+//   const webpage = io.of("/webpage");
+//   webpage.on("connect", (socket) => {
+//     console.log("웹페이지 웹소켓 연결됨");
+//   });
+
+//   /* 가속도
+//   socket.on("acc", (msg) => {
+//     console.log(msg);
+//   });
+//   */
+
+//   socket.on("disconnect", () => {
+//     console.log("watch4 웹 소켓 연결 끊김");
+//   });
+// });
 
 const port = 3000;
 server.listen(port, () => {
@@ -69,3 +102,16 @@ server.listen(port, () => {
 });
 
 // 심박수 웹페이지에 표시
+
+arduinoSerialPort.on("open", () => {
+  console.log("Serial Port Open");
+});
+
+arduinoSerialPort.on("data", (data) => {
+  const accData = data.toString();
+  const parsedData = JSON.parse(accData);
+  fs.writeFileSync("first.json", parsedData);
+  //const dataBuffer = fs.readFileSync("first.json");
+  console.log(dataBuffer.toString());
+});
+// //가속도 데이터 get
